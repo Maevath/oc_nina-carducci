fb20de473facb538218e72135db8c5b150852fc4
maeva <maevatblt@outlook.fr>
2023-12-07 17:13:39 +0100

    minify

--- assets/scripts.js
+++ assets/scripts.js
@@ -13,14 +13,3 @@ $(document).ready(function () {
         tagsPosition: 'top'
     });
 });
-
-// Enregistrement du service worker (Mise en cache)
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
-        .then(registration => {
-            console.log('Service Worker registered with scope:', registration.scope);
-        })
-        .catch(error => {
-            console.error('Service Worker registration failed:', error);
-        });
-}
